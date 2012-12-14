e26f0cedc3d00684eed38721a7e25fdf061902b5
Ali Yakamercan <ayakamercan@chitika.com>
2012-12-14 01:36:38 -0500

    changes for pooling connections

--- src/cberl.erl
+++ src/cberl.erl
@@ -6,28 +6,33 @@
 
 -module(cberl).
 -include("cberl.hrl").
--export([new/0, new/1, new/3, new/4, remove/2, destroy/1]).
--export([store/7, mget/3, getl/3, arithmetic/6]).
+
+-export([start_link/2, start_link/3, start_link/5, start_link/6]).
+-export([stop/1]).
 %store operations
--export([add/4, add/5, replace/4, replace/5, set/4, set/5]).
+-export([add/4, add/5, replace/4, replace/5, set/4, set/5, store/7]).
 %update operations
 -export([append/4, prepend/4, touch/3, mtouch/3]).
 -export([incr/3, incr/4, incr/5, decr/3, decr/4, decr/5]).
+-export([arithmetic/6]).
 %retrieval operations
--export([get_and_touch/3, get_and_lock/3, mget/2, get/2, unlock/3]).
+-export([get_and_touch/3, get_and_lock/3, mget/2, get/2, unlock/3, 
+         mget/3, getl/3]).
+%remove
+-export([remove/2]).
 
 
-%% @equiv new("localhost:8091", "", "", "")
-new() ->
-    new("localhost:8091", "", "", "").
+%% @equiv start_link(PoolName, NumCon, "localhost:8091", "", "", "")
+start_link(PoolName, NumCon) ->
+    start_link(PoolName, NumCon, "localhost:8091", "", "", "").
 
-%% @equiv new(Host, "", "", "")
-new(Host) ->
-    new(Host, "", "", "").
+%% @equiv start_link(PoolName, NumCon, Host, "", "", "")
+start_link(PoolName, NumCon, Host) ->
+    start_link(PoolName, NumCon, Host, "", "", "").
 
-%% @equiv new(Host, Username, Password, "")
-new(Host, Username, Password) ->
-    new(Host, Username, Password, "").
+%% @equiv start_link(PoolName, NumCon, Host, Username, Password, "")
+start_link(PoolName, NumCon, Host, Username, Password) ->
+    start_link(PoolName, NumCon, Host, Username, Password, "").
 
 %% @doc Create an instance of libcouchbase
 %% hosts A list of hosts:port separated by ';' to the
@@ -38,121 +43,125 @@ new(Host, Username, Password) ->
 %% Username the username to use
 %% Password The password
 %% bucket The bucket to connect to
--spec new(string(), string(), string(), string()) -> {ok, instance()} | {error, _}.
-new(Host, Username, Password, BucketName) ->
-    new(Host, Username, Password, BucketName, cberl_transcoder).
+-spec start_link(atom(), integer(), string(), string(), string(), string()) -> {ok, instance()} | {error, _}.
+start_link(PoolName, NumCon, Host, Username, Password, BucketName) ->
+    start_link(PoolName, NumCon, Host, Username, Password, BucketName, cberl_transcoder).
 
 
-new(Host, Username, Password, BucketName, Transcoder) ->
-    {ok, Handle} = cberl_nif:new(Host, Username, Password, BucketName),
-    {ok, #instance{handle = Handle, transcoder = Transcoder}}.
+start_link(PoolName, NumCon, Host, Username, Password, BucketName, Transcoder) ->
+    SizeArgs = [{size, NumCon},
+                {max_overflow, 10}],
+    PoolArgs = [{name, {local, PoolName}},
+                {worker_module, cberl_worker}] ++ SizeArgs,
+    poolboy:start(PoolArgs, [Host, Username, Password, BucketName, Transcoder]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%% STORE OPERATIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%
 
-%% @equiv add(Instance, Key, Exp, Value, standard)
+%% @equiv add(PoolName, Key, Exp, Value, standard)
 -spec add(instance(), key(), integer(), value()) -> ok | {error, _}.
-add(Instance, Key, Exp, Value) ->
-    add(Instance, Key, Exp, Value, standard).
+add(PoolName, Key, Exp, Value) ->
+    add(PoolName, Key, Exp, Value, standard).
 
-%% @equiv store(Instance, add, Key, Value, TranscoderOpts, Exp, 0)
+%% @equiv store(PoolName, add, Key, Value, TranscoderOpts, Exp, 0)
 -spec add(instance(), key(), integer(), value(), atom()) -> ok | {error, _}.
-add(Instance, Key, Exp, Value, TranscoderOpts) ->
-    store(Instance, add, Key, Value, TranscoderOpts, Exp, 0).
+add(PoolName, Key, Exp, Value, TranscoderOpts) ->
+    store(PoolName, add, Key, Value, TranscoderOpts, Exp, 0).
 
-%% @equiv replace(Instance, Key, Exp, Value, standard)
+%% @equiv replace(PoolName, Key, Exp, Value, standard)
 -spec replace(instance(), key(), integer(), value()) -> ok | {error, _}.
-replace(Instance, Key, Exp, Value) ->
-    replace(Instance, Key, Exp, Value, standard).
+replace(PoolName, Key, Exp, Value) ->
+    replace(PoolName, Key, Exp, Value, standard).
 
-%% @equiv store(Instance, replace, "", Key, Value, Exp)
+%% @equiv store(PoolName, replace, "", Key, Value, Exp)
 -spec replace(instance(), key(), integer(), value(), atom()) -> ok | {error, _}.
-replace(Instance, Key, Exp, Value, TranscoderOpts) ->
-    store(Instance, replace, Key, Value, TranscoderOpts, Exp, 0).
+replace(PoolName, Key, Exp, Value, TranscoderOpts) ->
+    store(PoolName, replace, Key, Value, TranscoderOpts, Exp, 0).
 
-%% @equiv set(Instance, Key, Exp, Value, standard)
+%% @equiv set(PoolName, Key, Exp, Value, standard)
 -spec set(instance(), key(), integer(), value()) -> ok | {error, _}.
-set(Instance, Key, Exp, Value) ->
-    set(Instance, Key, Exp, Value, standard).
+set(PoolName, Key, Exp, Value) ->
+    set(PoolName, Key, Exp, Value, standard).
 
-%% @equiv store(Instance, set, "", Key, Value, Exp)
+%% @equiv store(PoolName, set, "", Key, Value, Exp)
 -spec set(instance(), key(), integer(), value(), integer()) -> ok | {error, _}.
-set(Instance, Key, Exp, Value, TranscoderOpts) ->
-    store(Instance, set, Key, Value, TranscoderOpts, Exp, 0).
+set(PoolName, Key, Exp, Value, TranscoderOpts) ->
+    store(PoolName, set, Key, Value, TranscoderOpts, Exp, 0).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%% UPDATE OPERATIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec append(instance(), integer(), key(), value()) -> ok | {error, _}.
-append(Instance, Cas, Key, Value) ->
-    store(Instance, append, Key, Value, str, 0, Cas).
+append(PoolName, Cas, Key, Value) ->
+    store(PoolName, append, Key, Value, str, 0, Cas).
 
 -spec prepend(instance(), integer(), key(), value()) -> ok | {error, _}.
-prepend(Instance, Cas, Key, Value) ->
-    store(Instance, prepend, Key, Value, str, 0, Cas).
+prepend(PoolName, Cas, Key, Value) ->
+    store(PoolName, prepend, Key, Value, str, 0, Cas).
 
 %% @doc Touch (set expiration time) on the given key
-%% Instance libcouchbase instance to use
+%% PoolName libcouchbase instance to use
 %% Key key to touch
 %% ExpTime a new expiration time for the item 
 -spec mtouch(instance(), key(), integer()) -> ok | {error, _}.
-touch(Instance, Key, ExpTime) ->
-    {ok, Return} = mtouch(Instance, [Key], [ExpTime]),
+touch(PoolName, Key, ExpTime) ->
+    {ok, Return} = mtouch(PoolName, [Key], [ExpTime]),
     {ok, hd(Return)}.
 
-mtouch(Instance, Keys, ExpTime) when is_integer(ExpTime) ->
-    mtouch(Instance, Keys, [ExpTime]);
-mtouch(#instance{handle = Handle}, Keys, ExpTimes) ->
+mtouch(PoolName, Keys, ExpTime) when is_integer(ExpTime) ->
+    mtouch(PoolName, Keys, [ExpTime]);
+mtouch(PoolName, Keys, ExpTimes) ->
     ExpTimesE = case length(Keys) - length(ExpTimes) of
         R when R > 0 ->
             ExpTimes ++ lists:duplicate(R, lists:last(ExpTimes));
         _ -> 
             ExpTimes
     end, 
-    cberl_nif:mtouch(Handle, Keys, ExpTimesE).
+    execute(PoolName, {mtouch, Keys, ExpTimesE}).
 
-incr(Instance, Key, OffSet) ->
-    arithmetic(Instance, Key, OffSet, 0, 0, 0).
+incr(PoolName, Key, OffSet) ->
+    arithmetic(PoolName, Key, OffSet, 0, 0, 0).
 
-incr(Instance, Key, OffSet, Default) ->
-    arithmetic(Instance, Key, OffSet, 0, 1, Default).
+incr(PoolName, Key, OffSet, Default) ->
+    arithmetic(PoolName, Key, OffSet, 0, 1, Default).
 
-incr(Instance, Key, OffSet, Default, Exp) ->
-    arithmetic(Instance, Key, OffSet, Exp, 1, Default).
+incr(PoolName, Key, OffSet, Default, Exp) ->
+    arithmetic(PoolName, Key, OffSet, Exp, 1, Default).
 
-decr(Instance, Key, OffSet) ->
-    arithmetic(Instance, Key, -OffSet, 0, 0, 0).
+decr(PoolName, Key, OffSet) ->
+    arithmetic(PoolName, Key, -OffSet, 0, 0, 0).
 
-decr(Instance, Key, OffSet, Default) ->
-    arithmetic(Instance, Key, -OffSet, 0, 1, Default).
+decr(PoolName, Key, OffSet, Default) ->
+    arithmetic(PoolName, Key, -OffSet, 0, 1, Default).
 
-decr(Instance, Key, OffSet, Default, Exp) ->
-    arithmetic(Instance, Key, -OffSet, Exp, 1, Default).
+decr(PoolName, Key, OffSet, Default, Exp) ->
+    arithmetic(PoolName, Key, -OffSet, Exp, 1, Default).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%% RETRIEVAL METHODS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec get_and_touch(instance(), key(), integer()) -> {ok, integer(), value()} | {error, _}.
-get_and_touch(Instance, Key, Exp) -> 
-    mget(Instance, [Key], Exp).
+get_and_touch(PoolName, Key, Exp) -> 
+    mget(PoolName, [Key], Exp).
 
 -spec get(instance(), key()) -> {ok, integer(), value()} | {error, _}.
-get(Instance, Key) ->
-    hd(mget(Instance, [Key], 0)).
+get(PoolName, Key) ->
+    hd(mget(PoolName, [Key], 0)).
 
-mget(Instance, Keys) ->
-    mget(Instance, Keys, 0).
+mget(PoolName, Keys) ->
+    mget(PoolName, Keys, 0).
 
 -spec get_and_lock(instance(), key(), integer()) -> {ok, integer(), value()} | {error, _}.
-get_and_lock(Instance, Key, Exp) ->
-    getl(Instance, Key, Exp).
+get_and_lock(PoolName, Key, Exp) ->
+    getl(PoolName, Key, Exp).
 
 -spec unlock(instance(), key(), integer()) -> ok | {error, _}.
-unlock(#instance{handle = Handle}, Key, Cas) ->
-    cberl_nif:unlock(Handle, Key, Cas).
+unlock(PoolName, Key, Cas) ->
+    execute(PoolName, {unlock, Key, Cas}).
+
 %% @doc main store function takes care of all storing
 %% Instance libcouchbase instance to use
 %% Op add | replace | set | append | prepend
@@ -172,11 +181,10 @@ unlock(#instance{handle = Handle}, Key, Cas) ->
 %% CAS
 -spec store(instance(), operation_type(), key(), value(), atom(), 
             integer(), integer()) -> ok | {error, _}.
-store(#instance{handle = Handle, transcoder = Transcoder}, Op, Key, Value, TranscoderOpts, Exp, Cas) ->
-    StoreValue = Transcoder:encode_value(TranscoderOpts, Value), 
-    cberl_nif:store(Handle, operation_value(Op), Key, StoreValue, 
-                    Transcoder:flag(TranscoderOpts), Exp, Cas).
-
+store(PoolName, Op, Key, Value, TranscoderOpts, Exp, Cas) ->
+    execute(PoolName, {store, Op, Key, Value,
+                       TranscoderOpts, Exp, Cas}).
+    
 %% @doc get the value for the given key
 %% Instance libcouchbase instance to use
 %% HashKey the key to use for hashing 
@@ -184,34 +192,17 @@ store(#instance{handle = Handle, transcoder = Transcoder}, Op, Key, Value, Trans
 %% Exp When the object should expire
 %%      pass a negative number for infinity
 -spec mget(instance(), key(), integer()) -> {ok, integer(), value()} | {error, _}.
-mget(#instance{handle = Handle, transcoder = Transcoder}, Keys, Exp) ->
-    case cberl_nif:mget(Handle, Keys, Exp) of
-        {error, Error} -> {error, Error};
-        {ok, Results} ->
-            lists:map(fun(Result) ->
-                        case Result of
-                            {Cas, Flag, Key, Value} ->
-                                DecodedValue = Transcoder:decode_value(Flag, Value),
-                                {ok, Cas, DecodedValue};
-                            {_Key, {error, _Error}} ->
-                                Result
-                        end
-                end, Results)
-    end.
-
+mget(PoolName, Keys, Exp) ->
+    execute(PoolName, {mget, Keys, Exp}).
+    
 %% @doc Get an item with a lock that has a timeout
 %% Instance libcouchbase instance to use
 %%  HashKey the key to use for hashing
 %%  Key the key to get
 %%  Exp When the lock should expire
 -spec getl(instance(), key(), integer()) -> {ok, integer(), value()} | {error, _}.
-getl(#instance{handle = Handle, transcoder = Transcoder}, Key, Exp) ->
-    case cberl_nif:getl(Handle, Key, Exp) of
-        {error, Error} -> {error, Error};
-        {ok, {Cas, Flag, Value}} ->
-            DecodedValue = Transcoder:decode_value(Flag, Value),
-            {ok, Cas, DecodedValue}
-    end.
+getl(PoolName, Key, Exp) ->
+    execute(PoolName, {getl, Key, Exp}).
    
 %% @doc perform an arithmetic operation on the given key
 %% Instance libcouchbase instance to use
@@ -223,33 +214,20 @@ getl(#instance{handle = Handle, transcoder = Transcoder}, Key, Exp) ->
 %% Initial The initial value of the object if we create it       
 -spec arithmetic(instance(), key(), integer(), integer(), integer(), integer()) ->
    ok | {error, _}.
-arithmetic(#instance{handle = Handle, transcoder = Transcoder}, Key, OffSet, Exp, Create, Initial) ->
-    case cberl_nif:arithmetic(Handle, Key, OffSet, Exp, Create, Initial) of
-        {error, Error} -> {error, Error};
-        {ok, {Cas, Flag, Value}} ->
-            DecodedValue = Transcoder:decode_value(Flag, Value),
-            {ok, Cas, DecodedValue}
-    end.
+arithmetic(PoolName, Key, OffSet, Exp, Create, Initial) ->
+    execute(PoolName, {arithmetic, Key, OffSet, Exp, Create, Initial}).
 
 %% @doc remove the value for given key
 %% Instance libcouchbase instance to use
 %% Key key to  remove
 -spec remove(instance(), key()) -> ok | {error, _}.
-remove(#instance{handle = Handle}, Key) ->
-    cberl_nif:remove(Handle, Key, 0).
+remove(PoolName, Key) ->
+    execute(PoolName, {remove, Key, 0}).
 
-%% @doc close the libcouchbase instance
-destroy(#instance{handle = Handle}) ->
-    cberl_nif:destroy(Handle).
+stop(PoolName) ->
+    poolboy:stop(PoolName).
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% INTERNAL FUNCTIONS %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-
--spec operation_value(operation_type()) -> integer().
-operation_value(add) -> ?'CBE_ADD';
-operation_value(replace) -> ?'CBE_REPLACE';
-operation_value(set) -> ?'CBE_SET';
-operation_value(append) -> ?'CBE_APPEND';
-operation_value(prepend) -> ?'CBE_PREPEND'.
+execute(PoolName, Cmd) ->
+    poolboy:transaction(PoolName, fun(Worker) ->
+            gen_server:call(Worker, Cmd)
+       end).
