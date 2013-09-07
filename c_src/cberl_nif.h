449517100e0a80047451af92a791da7622f67b1c
William Cummings <wcummings22@gmail.com>
2013-09-07 11:54:54 -0400

    Added enum for c

--- c_src/cberl_nif.h
+++ c_src/cberl_nif.h
@@ -4,6 +4,17 @@
 #include "erl_nif.h"
 #include "cberl.h"
 
+// Command enum
+#define CMD_CONNECT     0
+#define CMD_STORE       1
+#define CMD_MGET        2
+#define CMD_GETL        3
+#define CMD_UNLOCK      4
+#define CMD_MTOUCH      5
+#define CMD_ARITHMETIC  6
+#define CMD_REMOVE      7
+#define CMD_HTTP        8 
+
 typedef struct task {
     ErlNifPid* pid;
     unsigned int cmd;
