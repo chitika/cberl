8070d81402e928e6fa10576b8f17bb4753daedc9
Philipp Fehre <philipp.fehre@googlemail.com>
2014-06-28 13:21:48 +0100

    move design doc creation to view tests
    create view tests file since there are more view tests to come and I
    think it's good to encapsulate this since the design doc handling is
    somewhat independent of the basic KV engine.

---
+++ test/cberl_view_tests.erl
@@ -0,0 +1,52 @@
+-module(cberl_view_tests).
+-include_lib("eunit/include/eunit.hrl").
+-define(POOLNAME, testpool).
+
+cberl_view_test_() ->
+    [{foreach, fun setup/0, fun clean_up/1,
+      [fun test_set_design_doc/1,
+       fun test_remove_design_doc/1
+      ]}].
+
+
+%%%===================================================================
+%%% Setup / Teardown
+%%%===================================================================
+
+setup() ->
+    cberl:start_link(?POOLNAME, 3),
+    cberl:set_design_doc(?POOLNAME, "test-design-doc",
+                         {[{<<"views">>,
+                            {[{<<"test-view">>,
+                               {[{<<"map">>, <<"function(doc,meta){}">>}]}
+                              }]}
+                           }]}),
+    ok.
+
+clean_up(_) ->
+    cberl:stop(?POOLNAME).
+
+%%%===================================================================
+%%% Tests
+%%%===================================================================
+
+test_set_design_doc(_) ->
+    DocName = "test-set-design-doc",
+    ViewName = "test-view",
+    DesignDoc = {[{<<"views">>,
+                   {[{list_to_binary(ViewName),
+                      {[{<<"map">>, <<"function(doc,meta){}">>}]}
+                     }]}
+                  }]},
+    fun () ->
+        [?assertEqual(ok, cberl:set_design_doc(?POOLNAME, DocName, DesignDoc)),
+         ?assertMatch({ok, _}, cberl:view(?POOLNAME, DocName, ViewName, []))]
+    end.
+
+test_remove_design_doc(_) ->
+    DocName = "test-design-doc",
+    ViewName = "test-view",
+    fun () ->
+        [?assertEqual(ok, cberl:remove_design_doc(?POOLNAME, DocName)),
+         ?assertMatch({error, _}, cberl:view(?POOLNAME, DocName, ViewName, []))]
+    end.
